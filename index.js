c35213c0e21a9646706e071c5853446b9d13739c
Myron Ingram <mingram8907@gmail.com>
2023-03-02 16:49:49 -0500

    day 3

--- index.js
+++ index.js
@@ -8,17 +8,24 @@ const liveDisplay = document.getElementById("Live")
 
 
 // generate random number to predict from
-const liveNum = Math.floor(Math.random() * 100) + 1
+let liveNum; 
+
 
 // add click event to deal button
 dealButton.addEventListener("click", function() {
-     
+    
+    random()
+    
     // log to test
     console.log(liveNum);
 
     // set live display to show the random liveNum
     liveDisplay.innerHTML = liveNum
 })
+
+function random() {
+    liveNum = Math.floor(Math.random() * 100) + 1;
+}
 
 
 // select and store the new display in variable
@@ -42,33 +49,130 @@ higherButton.addEventListener("click", function() {
     // set new display to show the random newNum
     newDisplay.innerHTML = newNum
 
-    // determine the round's winner
-    whoWinsRoundHigher()
-})
+    playerScore = Math.abs(newNum - liveNum)
+    computerScore = Math.abs(newNum - liveNum)
 
-// define how to determine the round's winner if higher is selected
-function whoWinsRoundHigher() {
+    // determine the round's winner
     if (newNum > liveNum) {
         console.log("You win");
+        result.innerHTML = "Correct! You win " + playerScore + " points!"
+        // playerWinsRound()
+    }
+    else if (liveNum > newNum) {
+        console.log("Computer wins");
+        result.innerHTML = "Wrong! Computer wins " + playerScore + " points!"
+        // computerWinsRound()
     }
     else {
-        console.log("Computer wins");
+        console.log("Tie");
+        result.innerHTML = "Tie! No winner. Play again!"
     }
-}
+
+    // reset round -- create another button and assign as click event
+    // setTimeout(resetRound(), 5000)
+})
+
+
+// select and store the player score and computer score in separate variables
+let playerScore;
+
+let computerScore;
+
+
+// define point calculator
+// let pointCalculator = Math.abs(newNum - liveNum)
+
+
+// define scoring system for player
+// function playerWinsRound() {
+    
+//     playerScore = Math.abs(newNum - liveNum)
+    
+//     document.getElementById("Player").innerHTML = playerScore
+
+//     if (newNum > liveNum) {
+//         console.log("You win");
+//         result.innerHTML = "Correct! You win " + playerScore + " points!"
+//         playerWinsRound()
+//     }
+//     else if (newNum < liveNum) {
+//         console.log("You win");
+//         result.innerHTML = "Correct! You win " + playerScore + " points!"
+//         playerWinsRound() 
+//     }
+//     else {
+//         console.log("Tie");
+//         result.innerHTML = "Tie! No winner. Play again!"
+//     }
+// }
+
+
+// define scoring system for computer
+// function computerWinsRound() {
+//     playerScore = Math.abs(newNum - liveNum)
+    
+//     document.getElementById("Computer").innerHTML = computerScore
+
+//     if (liveNum > newNum) {
+//         console.log("Computer wins");
+//         result.innerHTML = "Wrong! Computer wins " + playerScore + " points!"
+//         computerWinsRound()
+//     }
+//     else if (liveNum < newNum) {
+//         console.log("Computer wins");
+//         result.innerHTML = "Wrong! Computer wins " + playerScore + " points!"
+//         computerWinsRound() 
+//     }
+//     else {
+//         console.log("Tie");
+//         result.innerHTML = "Tie! No winner. Play again!"
+//     }
+// }
+
+// select and store result in variable
+const result = document.getElementById("Result")
+
+
+// define how to determine the round's winner if higher is selected
+// function whoWinsRoundHigher() {
+    // if (newNum > liveNum) {
+    //     console.log("You win");
+    //     result.innerHTML = "Correct! You win " + playerScore + " points!"
+    //     playerWinsRound()
+    // }
+    // else if (liveNum > newNum) {
+    //     console.log("Computer wins");
+    //     result.innerHTML = "Wrong! Computer wins " + playerScore + " points!"
+    //     computerWinsRound()
+    // }
+    // else {
+    //     console.log("Tie");
+    //     result.innerHTML = "Tie! No winner. Play again!"
+    // }
+// }
+
 
 // select and store the lower button in variable
 const lowerButton = document.getElementById("Lower")
 
 
 // define how to determine the round's winner if lower is selected
-function whoWinsRoundLower() {
-    if (newNum < liveNum) {
-        console.log("You win");
-    }
-    else {
-        console.log("Computer wins");
-    }
-}
+// function whoWinsRoundLower() {
+    // if (newNum < liveNum) {
+    //     console.log("You win");
+    //     result.innerHTML = "Correct! You win " + playerScore + " points!"
+    //     playerWinsRound()
+    // }
+    // else if (liveNum < newNum) {
+    //     console.log("Computer wins");
+    //     result.innerHTML = "Wrong! Computer wins " + playerScore + " points!"
+    //     computerWinsRound()
+    // }
+    // else {
+    //     console.log("Tie");
+    //     result.innerHTML = "Tie! No winner. Play again!"
+    // }
+// }
 
 
 // add click event to lower button
@@ -80,10 +184,38 @@ lowerButton.addEventListener("click", function() {
     // set new display to show the random newNum
     newDisplay.innerHTML = newNum
 
+    playerScore = Math.abs(newNum - liveNum)
+    computerScore = Math.abs(newNum - liveNum)
+
     // determine the round's winner
-    whoWinsRoundLower()
+    if (newNum < liveNum) {
+        console.log("You win");
+        result.innerHTML = "Correct! You win " + playerScore + " points!"
+        // playerWinsRound()
+    }
+    else if (liveNum < newNum) {
+        console.log("Computer wins");
+        result.innerHTML = "Wrong! Computer wins " + playerScore + " points!"
+        // computerWinsRound()
+    }
+    else {
+        console.log("Tie");
+        result.innerHTML = "Tie! No winner. Play again!"
+    }
+
+    // reset round
+    // setTimeout(resetRound(), 5000)
 })
 
+function resetRound() {
+    // clear live display
+    liveDisplay.innerHTML = ""
+
+    // clear new display
+    newDisplay.innerHTML = 
+
+    // clear result
+    result.innerHTML = ""
 
 
-
+}
